5dd50227cfec6e9031c0c84803d3e38e572fd9ac
Tomasevic Nikola <tomasevic444@gmail.com>
2023-05-18 19:58:18 +0200

    Pocetak ubacivanje u listu,klasa,slike

--- Zadatak1/MainWindow.xaml.cs
+++ Zadatak1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,22 @@ namespace Zadatak1
     {
         public MainWindow()
         {
+            this.DataContext = this;
+            List<lokacija> l = new List<lokacija>();
+            l.Add(new lokacija { Id = "1", Grad = "Novi Sad", Drzava = "Srbija", Logo = "/Logoi/novisad.png" } );
+            l.Add(new lokacija { Id = "2", Grad = "Beograd", Drzava = "Srbija", Logo = "/Logoi/beograd.png" });
+            l.Add(new lokacija { Id = "3", Grad = "Nis", Drzava = "Srbija", Logo = "/Logoi/nis.png" });
+            l.Add(new lokacija { Id = "4", Grad = "Subotica", Drzava = "Srbija", Logo = "/Logoi/subotica.png" });
+            l.Add(new lokacija { Id = "5", Grad = "Leskovac", Drzava = "Srbija", Logo = "/Logoi/leskovac.png" });
+
+            lokacije = new ObservableCollection<lokacija>(l);
             InitializeComponent();
         }
+        public ObservableCollection<lokacija> lokacije
+        {
+            get;
+            set;
+        }
+
     }
 }
---
+++ Zadatak1/lokacija.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Zadatak1
+{
+    public class lokacija : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        private string id;
+        private string grad;
+        private string drzava;
+        private string logo;
+       
+        
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value != id)
+                {
+                    id = value;
+                    OnPropertyChanged("id");
+                }
+            }
+        }
+
+        public string Grad
+        {
+            get
+            {
+                return grad;
+            }
+            set
+            {
+                if (value != grad)
+                {
+                    grad = value;
+                    OnPropertyChanged("Grad");
+                }
+            }
+        }
+
+        public string Drzava
+        {
+            get
+            {
+                return drzava;
+            }
+            set
+            {
+                if (value != drzava)
+                {
+                    drzava = value;
+                    OnPropertyChanged("Drzava");
+                }
+            }
+        }
+        public string Logo
+        {
+            get
+            {
+                return logo;
+            }
+            set
+            {
+                if (value != logo)
+                {
+                    logo = value;
+                    OnPropertyChanged("Logo");
+                }
+            }
+        }
+
+
+    }
+}
